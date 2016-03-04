9074a360adb4d5760f690f2ad75af5b49dfeb778
Paul Walker <pjwalker76@gmail.com>
2016-03-04 16:58:19 -0500

    Add module ex

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -1,7 +1,16 @@
+import Lib from '../src/module.js';
+
+console.log('>>',Lib);
 describe('1+1', () => {
   let sum = 1+1;
 
   it('should be 2', () => {
     expect(2).toBe(2);
+  });
+});
+
+describe('Test library', () => {
+  it('foo should foo-ize str', () => {
+    expect(Lib.foo('Hello')).toBe('Hello foo');
   });
 });
